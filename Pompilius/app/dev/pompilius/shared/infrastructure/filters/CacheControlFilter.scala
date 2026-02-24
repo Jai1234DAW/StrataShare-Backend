d085aa9a86f1bbf3e1b9941cd7e3b82bcee39492
Jaineth Esther Galicia Medina <jgalicia@trynewroads.com>
2026-02-24 09:36:15 +0100

    Estructura inicail delÃ proyecto en develop

---
+++ Pompilius/app/dev/pompilius/shared/infrastructure/filters/CacheControlFilter.scala
@@ -0,0 +1,44 @@
+package dev.pompilius.shared.infrastructure.filters
+
+import org.apache.pekko.stream.Materializer
+import play.api.http._
+import play.api.mvc._
+
+import javax.inject.{Inject,Singleton}
+import scala.concurrent.{ExecutionContext, Future}
+
+@Singleton
+class CacheControlFilter @Inject()(implicit
+    val mat: Materializer,
+    ec: ExecutionContext
+) extends Filter {
+
+  private val MustRevalidate = "no-cache, no-store, must-revalidate"
+
+  def apply(
+      nextFilter: RequestHeader => Future[Result]
+  )(requestHeader: RequestHeader): Future[Result] = {
+
+    nextFilter(requestHeader).map { result =>
+      result.body.contentType match {
+        case _ if !requestHeader.method.equalsIgnoreCase(HttpVerbs.GET) =>
+          result.withHeaders(HeaderNames.CACHE_CONTROL -> MustRevalidate)
+
+        case Some(ContentTypes.JSON) =>
+          result.withHeaders(HeaderNames.CACHE_CONTROL -> MustRevalidate)
+
+        case _ if result.header.status != Status.OK =>
+          result.withHeaders(HeaderNames.CACHE_CONTROL -> MustRevalidate)
+
+        case Some(contentType) if contentType.startsWith("application/") =>
+          result.withHeaders(HeaderNames.CACHE_CONTROL -> MustRevalidate)
+
+        case _ if result.header.headers.contains(HeaderNames.CACHE_CONTROL) =>
+          result
+
+        case _ =>
+          result
+      }
+    }
+  }
+}
---
+++ Pompilius/app/dev/pompilius/shared/infrastructure/filters/NodeFilter.scala
@@ -0,0 +1,21 @@
+package dev.pompilius.shared.infrastructure.filters
+
+import org.apache.pekko.stream.Materializer
+import play.api.http._
+import play.api.mvc._
+import javax.inject.{Inject, Singleton}
+import scala.concurrent.{ExecutionContext, Future}
+
+@Singleton
+class NodeFilter @Inject() (Configuration: Configuration)(implicit
+    val mat: Materializer,
+    ec: ExecutionContext
+) extends Filter {
+
+  def apply(
+      nextFilter: RequestHeader => Future[Result]
+  )(requestHeader: RequestHeader): Future[Result] = {
+    nextFilter(requestHeader).map (_.withHeaders(Strings.X_NODE_ID -> Configuration.nodeId.toString)
+
+  }
+}
