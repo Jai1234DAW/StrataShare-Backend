1468a80277858b4ee6320a861c3b2e776ca52013
Jaineth Esther Galicia Medina <jgalicia@trynewroads.com>
2026-03-02 14:28:11 +0100

    Actualizacion en la clase VerboseException

--- Pompilius/app/dev/pompilius/shared/domain/VerboseException.scala
+++ Pompilius/app/dev/pompilius/shared/domain/VerboseException.scala
@@ -2,19 +2,16 @@ package dev.pompilius.shared.domain
 
 import java.util.concurrent.atomic.AtomicLong
 
-
 @SuppressWarnings(Array("NullAssignment"))
-class VerboseException(
-    val message: String,
-    val logAsError:Boolean=false) extends Exception(message) {
+class VerboseException(val message: String, val logAsError: Boolean = false) extends Exception(message) {
 
-  var id: String=VerboseException.nextId
+  var id: String = VerboseException.nextId
 }
 
 object VerboseException {
   private val generator = new AtomicLong(System.currentTimeMillis())
 
-  private def nextId: String={
+  private def nextId: String = {
     java.lang.Long.toString(generator.incrementAndGet(), 36)
   }
 }
