ea6ebfff4d19b08fab5448d3c8a1802d3d802556
Jaineth Esther Galicia Medina <jgalicia@trynewroads.com>
2026-03-02 14:02:56 +0100

    Realizacion de la clase Verbose Exception para excepciones descriptivas

---
+++ Pompilius/app/dev/pompilius/shared/domain/VerboseException.scala
@@ -0,0 +1,20 @@
+package dev.pompilius.shared.domain
+
+import java.util.concurrent.atomic.AtomicLong
+
+
+@SuppressWarnings(Array("NullAssignment"))
+class VerboseException(
+    val message: String,
+    val logAsError:Boolean=false) extends Exception(message) {
+
+  var id: String=VerboseException.nextId
+}
+
+object VerboseException {
+  private val generator = new AtomicLong(System.currentTimeMillis())
+
+  private def nextId: String={
+    java.lang.Long.toString(generator.incrementAndGet(), 36)
+  }
+}
