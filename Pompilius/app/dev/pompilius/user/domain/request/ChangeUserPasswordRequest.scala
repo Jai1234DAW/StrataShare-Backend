39319d7ddfcf74558680bfed6fe63cba0f11063c
Jaineth Esther Galicia Medina <jgalicia@trynewroads.com>
2026-03-09 09:43:17 +0100

    Correcion de formateo de la clase ChangeUserPasswordRequest.scala

--- Pompilius/app/dev/pompilius/user/domain/request/ChangeUserPasswordRequest.scala
+++ Pompilius/app/dev/pompilius/user/domain/request/ChangeUserPasswordRequest.scala
@@ -2,5 +2,5 @@ package dev.pompilius.user.domain.request
 
 case class ChangeUserPasswordRequest (oldPassword:String,
                                       newPassword:String,
-                                      closeAllSessions: Boolean,)
+                                      closeAllSessions: Boolean)
 
