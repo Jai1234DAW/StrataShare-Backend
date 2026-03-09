b13e3f356f25c9a1a99632e6911d7c56a2281961
Jaineth Esther Galicia Medina <jgalicia@trynewroads.com>
2026-03-09 14:14:25 +0100

    Correccion de la clase SessionRepository y sus respectivos metodos

--- Pompilius/app/dev/pompilius/auth/domain/SessionRepository.scala
+++ Pompilius/app/dev/pompilius/auth/domain/SessionRepository.scala
@@ -1,14 +1,16 @@
 package dev.pompilius.auth.domain
 
 import com.google.inject.ImplementedBy
+import dev.pompilius.auth.infrastructure.repositories.SessionMySqlRepository
 import dev.pompilius.user.domain.UserId
 import org.apache.pekko.Done
-
+import dev.pompilius.shared.domain.Pagination
 import scala.concurrent.Future
 
 @ImplementedBy(classOf[SessionMySqlRepository])
 trait SessionRepository {
-  def findSession(userId: UserId, token: SessionToken): Future[Option[Session]]
+  def findById(sessionId: SessionId): Future[Option[Session]]
+  def find(filter: SessionFilter, pag: Pagination): Future[List[Session]]
   def save(session: Session): Future[Done]
-  def closeAllSessions(userId: UserId, Keep: Option[SessionToken]): Future[Done]
+  def closeAllSessions(userId: UserId, Keep: Option[SessionId]): Future[Done]
 }
