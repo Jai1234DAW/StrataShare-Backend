f07e955825afe3b5a189871b7253d187fe87dacb
Jaineth Esther Galicia Medina <jgalicia@trynewroads.com>
2026-03-18 08:16:13 +0100

    Clase creada para representar una lista de elementos paginada

---
+++ Pompilius/app/dev/pompilius/shared/domain/Paginated.scala
@@ -0,0 +1,17 @@
+package dev.pompilius.shared.domain
+
+
+case class Paginated[T](items: List[T], moreItems: Boolean)
+
+object Paginated {
+
+  def apply[T](items: List[T], pag: Pagination): Paginated[T] = {
+    pag.limit match {
+      case Some(limit) =>
+        Paginated(items = items.take(limit), items.length > limit)
+      case _ =>
+        Paginated(items = items, moreItems = false)
+    }
+  }
+
+}
