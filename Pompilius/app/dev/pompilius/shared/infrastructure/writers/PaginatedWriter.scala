6622ab11bb49daf6b8442fe838ed0c3141c27c32
Jaineth Esther Galicia Medina <jgalicia@trynewroads.com>
2026-03-18 08:23:36 +0100

    Creacion de la clase Paginated Writer para convertir una lista paginada de elementos a JSON

---
+++ Pompilius/app/dev/pompilius/shared/infrastructure/writers/PaginatedWriter.scala
@@ -0,0 +1,29 @@
+package dev.pompilius.shared.infrastructure.writers
+
+import com.google.inject.ImplementedBy
+import play.api.libs.json._
+import dev.pompilius.shared.domain.Paginated
+
+import javax.inject.{Inject, Singleton}
+import scala.concurrent.{ExecutionContext, Future}
+
+@ImplementedBy(classOf[PaginatedWriterImpl])
+trait PaginatedWriter {
+  def toJson[T](p: Paginated[T])(implicit f: T => Future[JsValue]): Future[JsValue]
+  def toFlattedJson[T](p: Paginated[T])(implicit f: T => Future[Option[JsValue]]): Future[JsValue]
+}
+
+@Singleton
+class PaginatedWriterImpl @Inject() (implicit val ec: ExecutionContext) extends PaginatedWriter {
+
+  implicit val jsonWrites: Format[Paginated[JsValue]] = Json.format[Paginated[JsValue]]
+
+  override def toJson[T](p: Paginated[T])(implicit f: T => Future[JsValue]): Future[JsValue] = {
+    Future.sequence(p.items.map(item => f(item))).map(items => Json.toJson(Paginated(items = items, p.moreItems)))
+  }
+
+  def toFlattedJson[T](p: Paginated[T])(implicit f: T => Future[Option[JsValue]]): Future[JsValue] = {
+    Future.sequence(p.items.map(item => f(item))).map(items => Json.toJson(Paginated(items = items.flatten, p.moreItems)))
+  }
+
+}
