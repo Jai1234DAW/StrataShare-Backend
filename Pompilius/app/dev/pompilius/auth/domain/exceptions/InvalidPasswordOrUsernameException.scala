1decd90bcefb46761631d7f36f5d2c3a6f6e9cc6
Jaineth Esther Galicia Medina <jgalicia@trynewroads.com>
2026-03-23 20:51:27 +0100

    Creacion de excepcion para usuario o contraseña invalida

--- Pompilius/app/dev/pompilius/auth/domain/exceptions/InvalidPasswordOrUsernameException.scala
+++ Pompilius/app/dev/pompilius/auth/domain/exceptions/InvalidPasswordOrUsernameException.scala
@@ -2,5 +2,5 @@ package dev.pompilius.auth.domain.exceptions
 
 import dev.pompilius.shared.domain.exceptions.UnauthorizedException
 
-class InvalidPasswordOrUserNameException (message:String="Incorrect username or password") extends UnauthorizedException(message)
+class InvalidPasswordOrUsernameException(message:String="Incorrect username or password") extends UnauthorizedException(message)
 
