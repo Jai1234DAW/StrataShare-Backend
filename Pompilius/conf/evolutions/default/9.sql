900089cb3ab87fb4b4f41d01261d2027d5682130
Jaineth Esther Galicia Medina <jgalicia@trynewroads.com>
2026-03-24 17:47:03 +0100

    Modificacion en la configuracion y rutas

--- Pompilius/conf/evolutions/default/9.sql
+++ Pompilius/conf/evolutions/default/9.sql
@@ -1,7 +1,23 @@
-# Edicion de la tabla Request-Log para establecer la foreign key
+# Edicion de la tabla Request-Log para establecer la foreign key y creacion de la tabla mailsent
 
 # --- !Ups
 
 ALTER TABLE `request_log`
     ADD CONSTRAINT `fk_request_log_user`
         FOREIGN KEY (`user_id`) REFERENCES `users`(`id`);
+
+CREATE TABLE `mail_sent` (
+     `id` BIGINT NOT NULL,
+     `mail_type` ENUM('WELCOME') NOT NULL,
+     `address` VARCHAR(256) NOT NULL,
+     `sent_at` DATETIME NOT NULL DEFAULT current_timestamp,
+     `user_id` BIGINT NOT NULL,
+     `metadata` TEXT NULL,
+     PRIMARY KEY (`id`),
+     INDEX `MAIL_TYPE_IDX` (`mail_type` ASC),
+     INDEX `PERSON_IDX` (`user_id` ASC),
+     INDEX `ADDRESS_IDX` (`address` ASC),
+    CONSTRAINT `fk_mail_sent_user` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
+      ON DELETE CASCADE
+      ON UPDATE CASCADE
+)CHARSET=utf8mb4;
