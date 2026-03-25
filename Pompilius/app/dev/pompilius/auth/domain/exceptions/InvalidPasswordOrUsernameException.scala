9052b1d4156e4bd177ad8093cd95335e1a1e493b
Jaineth Esther Galicia Medina <jgalicia@trynewroads.com>
2026-03-25 13:09:07 +0100

    Modificaciones menores

--- Pompilius/app/dev/pompilius/auth/domain/exceptions/InvalidPasswordOrUsernameException.scala
+++ Pompilius/app/dev/pompilius/auth/domain/exceptions/InvalidPasswordOrUsernameException.scala
@@ -2,5 +2,5 @@ package dev.pompilius.auth.domain.exceptions
 
 import dev.pompilius.shared.domain.exceptions.UnauthorizedException
 
-class InvalidPasswordOrUsernameException(message:String="Incorrect username or password") extends UnauthorizedException(message)
+class InvalidPasswordOrUsernameException extends UnauthorizedException(message)
 
