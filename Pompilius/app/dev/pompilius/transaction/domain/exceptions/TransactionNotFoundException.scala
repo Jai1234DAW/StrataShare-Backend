cf045bda579695e5658cb851dffe3785ae8738c2
Jaineth Esther Galicia Medina <jgalicia@trynewroads.com>
2026-04-10 08:18:16 +0200

    Modificacion modulo transaction, creacion de excepciones e implementacion del Transaction Servicio

---
+++ Pompilius/app/dev/pompilius/transaction/domain/exceptions/TransactionNotFoundException.scala
@@ -0,0 +1,12 @@
+package dev.pompilius.transaction.domain.exceptions
+
+import dev.pompilius.shared.domain.VerboseException
+import dev.pompilius.transaction.domain.TransactionId
+
+class TransactionNotFoundException(message: String = "Resource not found") extends VerboseException(message = message)
+
+object TransactionNotFoundException {
+  def apply(transactionId:TransactionId): TransactionNotFoundException = {
+    new TransactionNotFoundException(s"Sample with id=${transactionId.toString} not found")
+  }
+}
