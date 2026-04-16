e7fdb91a6ade6db53190cd181123aa7350a1c3ff
Jaineth Esther Galicia Medina <jgalicia@trynewroads.com>
2026-04-16 14:47:49 +0200

    Modificacion del modulo barter

--- Pompilius/app/dev/pompilius/barter/infrastructure/writers/barterWriter.scala
+++ Pompilius/app/dev/pompilius/barter/infrastructure/writers/barterWriter.scala
@@ -3,7 +3,7 @@ package dev.pompilius.barter.infrastructure.writers
 import com.google.inject.ImplementedBy
 import dev.pompilius.Strings
 import dev.pompilius.barter.domain.Barter
-import dev.pompilius.shared.infrastructure.JsUtils.toJsValueWrapper
+import dev.pompilius.shared.infrastructure.JsUtils.{JodaDateTimeFormat,toJsValueWrapper}
 import dev.pompilius.transaction.domain.Transaction
 import play.api.libs.json._
 
@@ -12,47 +12,71 @@ import scala.concurrent.{ExecutionContext, Future}
 
 @ImplementedBy(classOf[BarterWriterImpl])
 trait BarterWriter {
-  def asJson(barter: Barter, transaction: Transaction): Future[JsValue]
-  def asJsonComplete(
-      barter: Barter,
-      transaction: Transaction
-  ): Future[JsValue]
+  def toJson(transaction: Transaction, barter: Barter): Future[JsValue]
+  def asBuyer(transaction: Transaction, barter: Barter): Future[JsValue]
+  def asSeller(transaction: Transaction, barter: Barter): Future[JsValue]
+  def asAdmin(transaction: Transaction, barter: Barter): Future[JsValue]
 }
 
 @Singleton
 class BarterWriterImpl @Inject() (implicit ec: ExecutionContext) extends BarterWriter {
 
-  override def asJson(barter: Barter, transaction: Transaction): Future[JsObject] = {
+  override def toJson(transaction: Transaction, barter: Barter): Future[JsValue] = {
     Future.successful {
-      Json.obj(
-        List(
-          toJsValueWrapper(Strings.barterId -> barter.barterId.toString),
-          toJsValueWrapper(Strings.transactionId -> barter.transactionId.toString),
-          toJsValueWrapper(Strings.transactionType -> transaction.transactionType.toString),
-          toJsValueWrapper(Strings.transactionStatus -> transaction.transactionStatus.toString),
-          toJsValueWrapper(Strings.offeredResourceId -> barter.offeredResourceId.toString),
-          toJsValueWrapper(Strings.requestedResourceId -> transaction.resourceId.toString),
-          toJsValueWrapper(Strings.created -> transaction.created.toString)
-        ).flatten: _*
+      Json.toJson(
+        Json.obj(
+          List(
+            toJsValueWrapper(Strings.barterId, barter.barterId.toString),
+            toJsValueWrapper(Strings.transactionId, barter.transactionId.toString),
+            toJsValueWrapper(Strings.transactionType, transaction.transactionType.toString),
+            toJsValueWrapper(Strings.transactionStatus, transaction.transactionStatus.toString),
+            toJsValueWrapper(Strings.offeredResourceId, barter.offeredResourceId.toString),
+            toJsValueWrapper(Strings.requestedResourceId, transaction.resourceId.toString),
+            toJsValueWrapper(Strings.created, transaction.created)
+          ).flatten: _*
+        )
       )
-
     }
   }
-  //MIRAR ESTO OJO
 
-  override def asJsonComplete(
-      barter: Barter,
-      transaction: Transaction
-  ): Future[JsValue] = {
+  override def asBuyer(transaction: Transaction, barter: Barter): Future[JsValue] = {
     for {
-      baseJson <- asJson(barter, transaction)
+      baseJson <- toJson(transaction, barter)
     } yield {
-      baseJson ++ Json.obj(
+      baseJson.as[JsObject] ++ Json.obj(
         List(
-          toJsValueWrapper(Strings.sellerId -> transaction.sellerId.toString),
-          toJsValueWrapper(Strings.buyerId -> transaction.buyerId.toString),
-          toJsValueWrapper(Strings.updated -> transaction.updated.toString),
-          toJsValueWrapper(Strings.completedAt -> transaction.completedAt.map(_.toString).getOrElse(""))
+          toJsValueWrapper(Strings.sellerId, transaction.sellerId.toString),
+          toJsValueWrapper(Strings.updated, transaction.updated),
+          toJsValueWrapper(Strings.completedAt, transaction.completedAt)
+        ).flatten: _*
+      )
+    }
+  }
+
+  override def asSeller(transaction: Transaction, barter: Barter): Future[JsValue] = {
+    for {
+      baseJson <- toJson(transaction, barter)
+    } yield {
+      baseJson.as[JsObject] ++ Json.obj(
+        List(
+          toJsValueWrapper(Strings.buyerId, transaction.buyerId.toString),
+          toJsValueWrapper(Strings.updated, transaction.updated),
+          toJsValueWrapper(Strings.completedAt, transaction.completedAt)
+        ).flatten: _*
+      )
+    }
+  }
+
+  override def asAdmin(transaction: Transaction, barter: Barter): Future[JsValue] = {
+    for {
+      baseJson <- toJson(transaction, barter)
+    } yield {
+      baseJson.as[JsObject] ++ Json.obj(
+        List(
+          toJsValueWrapper(Strings.sellerId, transaction.sellerId.toString),
+          toJsValueWrapper(Strings.buyerId, transaction.buyerId.toString),
+          toJsValueWrapper(Strings.updated, transaction.updated),
+          toJsValueWrapper(Strings.completedAt, transaction.completedAt)
         ).flatten: _*
       )
     }
