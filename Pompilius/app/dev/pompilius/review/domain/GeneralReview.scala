14afcd0934c5efa2902335756fedcb0800420bc5
Jaineth Esther Galicia Medina <jgalicia@trynewroads.com>
2026-04-28 13:23:41 +0200

    Creacion de dos enpoints para crear samples y studies con attachments directamente

--- Pompilius/app/dev/pompilius/review/domain/GeneralReview.scala
+++
@@ -1,9 +0,0 @@
-package dev.pompilius.review.domain
-
-import dev.pompilius.resource.domain.ResourceId
-
-case class GeneralReview(
-    resourceId: ResourceId,
-    totalComments: Int,
-    ratingAverage: Double
-)
--- Pompilius/app/dev/pompilius/sample/infrastructure/controllers/SampleController.scala
+++ Pompilius/app/dev/pompilius/sample/infrastructure/controllers/SampleController.scala
@@ -1,9 +1,10 @@
 package dev.pompilius.sample.infrastructure.controllers
 
+import dev.pompilius.attachment.infrastructure.Attachments
 import dev.pompilius.badge.application.BadgeService
 import dev.pompilius.event.domain.EventU
-import dev.pompilius.resource.domain.exceptions.ResourceNotFoundException
 import dev.pompilius.resource.domain._
+import dev.pompilius.resource.domain.exceptions.ResourceNotFoundException
 import dev.pompilius.resource.infrastructure.ResourceAccessValidator
 import dev.pompilius.resource.infrastructure.writers.ResourceWriter
 import dev.pompilius.sample.domain.{Sample, SampleFilter, SampleId, SampleRepository}
@@ -12,8 +13,9 @@ import dev.pompilius.shared.domain.{Paginated, Pagination, Visibility}
 import dev.pompilius.shared.infrastructure.BaseController
 import dev.pompilius.shared.infrastructure.writers.PaginatedWriter
 import dev.pompilius.users.domain.{Role, UserId}
-import play.api.Logger
-import play.api.mvc.{Action, AnyContent}
+import play.api.libs.Files
+import play.api.mvc.{Action, AnyContent, MultipartFormData}
+import play.api.{Configuration, Logger}
 
 import javax.inject._
 import scala.concurrent.{ExecutionContext, Future}
@@ -29,7 +31,8 @@ class SampleController @Inject() (
     paginatedWriter: PaginatedWriter,
     badgeService: BadgeService
 )(implicit val ec: ExecutionContext)
-    extends BaseController {
+    extends BaseController
+    with Attachments {
 
   private val logger = Logger(this.getClass)
 
@@ -62,7 +65,7 @@ class SampleController @Inject() (
           val newSample = Sample(
             id = sampleId,
             resourceId = resourceId,
-            collectedDate=createSampleRequest.collectedDate,
+            collectedDate = createSampleRequest.collectedDate,
             minerals = createSampleRequest.minerals,
             collectionMethods = createSampleRequest.collectionMethods,
             isFresh = createSampleRequest.isFresh,
@@ -96,6 +99,7 @@ class SampleController @Inject() (
               )
             )
             //Llamo aquí a lo de eventos.
+
             // Registrar evento
             samplesBadges <- badgeService.registerEventAndCheckBadges(user.id, EventU.SAMPLE_UPLOADED)
 
@@ -159,6 +163,92 @@ class SampleController @Inject() (
       }
     }
 
+  def createWithAttachments: Action[MultipartFormData[Files.TemporaryFile]] =
+    Action.async(parse.multipartFormData) { implicit request =>
+      withAnyOfThisRoles(Seq(Role.STUDENT, Role.PROFESSIONAL)) {
+        case (_, user, _, _) =>
+
+          val createSampleRequest =
+            CreateSampleRequestParser.parseMultipart(request.body)
+
+          val files = request.body.files
+
+          val resourceId = ResourceId.gen(configuration.nodeId)
+          val sampleId = SampleId.gen(configuration.nodeId)
+
+          val newResource = Resource(
+            id = resourceId,
+            name = createSampleRequest.name,
+            resourceType = ResourceType.SAMPLE,
+            visibility = createSampleRequest.visibility,
+            created = clock.now,
+            updated = clock.now,
+            location = createSampleRequest.location,
+            observations = createSampleRequest.observations,
+            summary = createSampleRequest.summary,
+            price = createSampleRequest.price,
+            isBarter = createSampleRequest.isBarter
+          )
+
+          val newSample = Sample(
+            id = sampleId,
+            resourceId = resourceId,
+            collectedDate = createSampleRequest.collectedDate,
+            minerals = createSampleRequest.minerals,
+            collectionMethods = createSampleRequest.collectionMethods,
+            isFresh = createSampleRequest.isFresh,
+            sampleType = createSampleRequest.sampleType,
+            materialsUsed = createSampleRequest.materialsUsed,
+            sampleCategory = createSampleRequest.sampleCategory,
+            geologicalProcesses = createSampleRequest.geologicalProcesses
+          )
+
+          for {
+            _ <- resourceRepository.save(newResource)
+
+            _ <- sampleRepository.save(newSample).recoverWith {
+              case NonFatal(e) =>
+                resourceRepository.delete(resourceId).map(_ => throw e)
+            }
+
+            _ <- resourceUserRepository.save(
+              ResourceUser(
+                resourceId = resourceId,
+                userId = user.id,
+                resourceUserType = ResourceUserType.OWNER,
+                created = clock.now
+              )
+            )
+            attachments <- Future.sequence {
+              request.body.files.map { filePart =>
+                saveAsAttachment(
+                  user = user,
+                  id = None,
+                  resourceId = Some(resourceId),
+                  file = filePart.ref.path.toFile,
+                  originalFilename = filePart.filename,
+                  description = filePart.contentType.map { ct =>
+                    if (ct.startsWith("image/")) "image" else "file"
+                  },
+                  contentType = filePart.contentType
+                )
+              }
+            }
+
+            samplesBadges <- badgeService.registerEventAndCheckBadges(user.id, EventU.SAMPLE_UPLOADED)
+
+            _ = if (samplesBadges.nonEmpty) {
+              logger.info(
+                s"User ${user.id} earned ${samplesBadges.length} badge(s): ${samplesBadges.map(_.name).mkString(", ")}"
+              )
+            }
+
+            json <- resourceWriter.asPublic(newResource, Some(newSample), None)
+
+          } yield Ok(json)
+      }
+    }
+
   def get(sampleId: String): Action[AnyContent] =
     Action.async { implicit request =>
       withAuthenticatedUser {
@@ -215,15 +305,15 @@ class SampleController @Inject() (
     }
 
   def getAll(
-              name: Option[String],
-              sampleType: Option[String],
-              sampleCategory: Option[String],
-              isFresh: Option[Boolean],
-              visibility: Option[String],
-              location: Option[String],
-              userId: Option[String],
-              pag: Pagination
-            ): Action[AnyContent] =
+      name: Option[String],
+      sampleType: Option[String],
+      sampleCategory: Option[String],
+      isFresh: Option[Boolean],
+      visibility: Option[String],
+      location: Option[String],
+      userId: Option[String],
+      pag: Pagination
+  ): Action[AnyContent] =
     Action.async { implicit request =>
       withAuthenticatedUser {
         case (_, user, _) =>
@@ -281,27 +371,26 @@ class SampleController @Inject() (
   }
 
   def getAllMySamples(
-                       pag: Pagination
-                     ): Action[AnyContent] =
+      pag: Pagination
+  ): Action[AnyContent] =
     Action.async { implicit request =>
       withAuthenticatedUser {
         case (_, user, _) =>
           for {
-            samples <- sampleRepository.getMyAllSamplesAsOwner(
-                userId = user.id,
-            pag.oneMore)
+            samples <- sampleRepository.getMyAllSamplesAsOwner(userId = user.id, pag.oneMore)
 
             json <- paginatedWriter.toJson(Paginated(samples, pag)) { sample =>
               for {
-                resource <- resourceRepository
-                  .findById(sample.resourceId)
-                  .map(
-                    _.getOrElse(
-                      throw new ResourceNotFoundException(
-                        s"Resource not found for sample ${sample.id}"
+                resource <-
+                  resourceRepository
+                    .findById(sample.resourceId)
+                    .map(
+                      _.getOrElse(
+                        throw new ResourceNotFoundException(
+                          s"Resource not found for sample ${sample.id}"
+                        )
                       )
                     )
-                  )
 
                 json <- resourceWriter.asPublic(resource, Some(sample), None)
               } yield json
